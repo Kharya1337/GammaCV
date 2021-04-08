9961d2575dbfa3595e52f6ece4c7f9b32a78f780
Kharya1337 <ikharlamov21@gmail.com>
2021-04-08 12:13:38 +0300

    chore: updated meta tags

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -45,7 +45,7 @@ export default function MyApp({ Component, pageProps }) {
       <Head>
         <meta name="viewport" content="width=device-width,user-scalable=no,initial-scale=1,maximum-scale=1,minimum-scale=1" />
         <title>GammaCV</title>
-        {/* <meta property="og:title" content="GammaCV" /> */}
+        <meta property="og:title" content="GammaCV" />
         <meta name="description" content="GammaCV is a WebGL accelerated Computer Vision library for modern web applications." />
         <meta property="og:description" content="GammaCV is a WebGL accelerated Computer Vision library for modern web applications." />
         {/* <meta name="twitter:card" content="summary" />
